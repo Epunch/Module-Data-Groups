598944dc85d3dd32373194fc3a441eeff5dbfdbf
Meysam Razagh <msm.razagh@gmail.com>
2026-05-03 21:06:10 +0200

    test: convert todo tests to active tests for dedupe function

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -1,27 +1,17 @@
 const dedupe = require("./dedupe.js");
-/*
-Dedupe Array
 
-📖 Dedupe means **deduplicate**
+test("given an empty array, it returns an empty array", () => {
+  // If input is empty, output should be empty
+  expect(dedupe([])).toEqual([]);
+});
 
-In this kata, you will need to deduplicate the elements of an array
+test("given an array with no duplicates, it returns a copy of the original array", () => {
+  // If there are no duplicates, the array stays the same
+  expect(dedupe([1, 2, 3])).toEqual([1, 2, 3]);
+});
 
-E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
-E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
-E.g. dedupe([1, 2, 1]) target output: [1, 2]
-*/
-
-// Acceptance Criteria:
-
-// Given an empty array
-// When passed to the dedupe function
-// Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
-
-// Given an array with no duplicates
-// When passed to the dedupe function
-// Then it should return a copy of the original array
-
-// Given an array with strings or numbers
-// When passed to the dedupe function
-// Then it should remove the duplicate values, preserving the first occurence of each element
+test("given an array with strings or numbers, it removes duplicate values", () => {
+  // Test if duplicates like 'a' or '1' are removed
+  expect(dedupe(["a", "a", "b"])).toEqual(["a", "b"]);
+  expect(dedupe([1, 1, 2, 2, 3])).toEqual([1, 2, 3]);
+});
