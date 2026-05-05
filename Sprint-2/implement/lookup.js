313aa955eea35334ec001f111db0738cf5541cf0
Meysam Razagh <msm.razagh@gmail.com>
2026-05-05 10:39:16 +0200

    feat: implement createLookup to convert array pairs to an object

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,34 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  // Error handling: If input is not an array, return an empty object
+  if (!Array.isArray(countryCurrencyPairs)) {
+    return {};
+  }
+
+  // We create an empty object to store our pairs
+  const lookup = {};
+
+  // We loop through each pair in the array
+  for (const pair of countryCurrencyPairs) {
+    const country = pair[0]; // The first item is the country code
+    const currency = pair[1]; // The second item is the currency code
+
+    // We add this pair to our object
+    lookup[country] = currency;
+  }
+
+  return lookup;
 }
 
 module.exports = createLookup;
+
+// --- Manual Test ---
+const inputData = [
+  ["US", "USD"],
+  ["CA", "CAD"],
+  ["BE", "EUR"],
+];
+const result = createLookup(inputData);
+
+console.log("My Result:");
+console.log(result);
+// -------------------
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,35 +1,25 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", function () {
+  const input = [
+    ["US", "USD"],
+    ["CA", "CAD"],
+    ["GB", "GBP"],
+  ];
 
-/*
+  const expectedOutput = {
+    US: "USD",
+    CA: "CAD",
+    GB: "GBP",
+  };
 
-Create a lookup object of key value pairs from an array of code pairs
+  const result = createLookup(input);
 
-Acceptance Criteria:
+  // In Jest, we use toEqual to compare objects
+  expect(result).toEqual(expectedOutput);
+});
 
-Given
- - An array of arrays representing country code and currency code pairs
-   e.g. [['US', 'USD'], ['CA', 'CAD']]
-
-When
- - createLookup function is called with the country-currency array as an argument
-
-Then
- - It should return an object where:
- - The keys are the country codes
- - The values are the corresponding currency codes
-
-Example
-Given: [['US', 'USD'], ['CA', 'CAD']]
-
-When
-createLookup(countryCurrencyPairs) is called
-
-Then
-It should return:
- {
-   'US': 'USD',
-   'CA': 'CAD'
- }
-*/
+test("returns an empty object when passed an empty array", function () {
+  const result = createLookup([]);
+  expect(result).toEqual({});
+});
