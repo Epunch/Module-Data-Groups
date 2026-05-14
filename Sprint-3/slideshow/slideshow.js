6751d35b5de59c868c8d41a7c9f039f9e9675e43
Meysam Razagh <msm.razagh@gmail.com>
2026-05-14 13:34:44 +0200

    Complete image carousel slideshow

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,7 +4,7 @@ const images = [
   "./assets/cute-cat-c.jpg",
 ];
 
-// Write your code here
+// Select elements
 const image = document.querySelector("#carousel-img");
 
 const forwardBtn = document.querySelector("#forward-btn");
@@ -14,11 +14,19 @@ const autoForwardBtn = document.querySelector("#auto-forward");
 const autoBackwardBtn = document.querySelector("#auto-backward");
 
 const stopBtn = document.querySelector("#stop");
+
+// Current image index
 let currentIndex = 0;
 
+// Interval variable
+let intervalId;
+
+// Show image function
 function showImage() {
   image.src = images[currentIndex];
 }
+
+// Forward button
 forwardBtn.addEventListener("click", function () {
   currentIndex++;
 
@@ -28,6 +36,8 @@ forwardBtn.addEventListener("click", function () {
 
   showImage();
 });
+
+// Backward button
 backwardBtn.addEventListener("click", function () {
   currentIndex--;
 
@@ -36,4 +46,53 @@ backwardBtn.addEventListener("click", function () {
   }
 
   showImage();
+});
+
+// Disable auto buttons
+function disableAutoButtons() {
+  autoForwardBtn.disabled = true;
+  autoBackwardBtn.disabled = true;
+}
+
+// Enable auto buttons
+function enableAutoButtons() {
+  autoForwardBtn.disabled = false;
+  autoBackwardBtn.disabled = false;
+}
+
+// Auto forward
+autoForwardBtn.addEventListener("click", function () {
+  disableAutoButtons();
+
+  intervalId = setInterval(function () {
+    currentIndex++;
+
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+
+    showImage();
+  }, 2000);
+});
+
+// Auto backward
+autoBackwardBtn.addEventListener("click", function () {
+  disableAutoButtons();
+
+  intervalId = setInterval(function () {
+    currentIndex--;
+
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+
+    showImage();
+  }, 2000);
+});
+
+// Stop button
+stopBtn.addEventListener("click", function () {
+  clearInterval(intervalId);
+
+  enableAutoButtons();
 });
