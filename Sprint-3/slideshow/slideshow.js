d52f02fde2ed27b50c8e3c944ada6e11db1b1da5
Meysam Razagh <msm.razagh@gmail.com>
2026-05-14 13:20:11 +0200

    Select DOM elements

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,16 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+const image = document.querySelector("#carousel-img");
+
+const forwardBtn = document.querySelector("#forward-btn");
+const backwardBtn = document.querySelector("#backward-btn");
+
+const autoForwardBtn = document.querySelector("#auto-forward");
+const autoBackwardBtn = document.querySelector("#auto-backward");
+
+const stopBtn = document.querySelector("#stop");
